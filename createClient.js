ff3b907528e1edefbb00ff1fdb7569907a354435
Rovshen Berdimyradov <rovshen.berdimyradov@emergn.com>
2023-04-29 00:11:51 +0400

    1

--- createClient.js
+++ createClient.js
@@ -4,7 +4,11 @@ module.exports = (req, res) => {
   const { arg1, arg2 } = req.query;
 
   // Run the bash script with the specified arguments
-  const script = spawn('sh', ['openvpn-install-hardcode.sh', arg1, arg2]);
+  let script;
+  if (arg1)
+    script = spawn('sh', ['openvpn-install-hardcode.sh', arg1, arg2]);
+  else
+    script = spawn('sh', ['openvpn-install-hardcode.sh']);
 
   // Log any output from the script to the console
   script.stdout.on('data', (data) => {
@@ -18,6 +22,7 @@ module.exports = (req, res) => {
   // When the script finishes, send the output as the response to the client
   script.on('close', (code) => {
     console.log(`child process exited with code ${code}`);
-    res.status(200).send(`Script exited with code ${code}`);
+    res.json({res: code})
+    // res.status(200).send(`Script exited with code ${code}`);
   });
 };
